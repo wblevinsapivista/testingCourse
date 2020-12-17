f32c5a7bfe11fdef6fb0ea920ee948018efd989d
Wynn <wynn.blevins@apivista.com>
2020-12-17 14:22:36 -0500

    end of module 1, lesson 4

--- assertionLibrary.js
+++ assertionLibrary.js
@@ -1,13 +1,27 @@
 const { sum, sub } = require('./math');
 
-let expected, result;
-expected  = 10;
-result = sum(3, 7);
-expect(result).toBe(10);
 
-expected = 7;
-result = sub(10, 3);
-expect(result).toBe(7);
+test('sum adds numbers', () => {
+    let expected = 10;
+    let result = sum(3, 7);
+    expect(result).toBe(expected);
+});
+
+test('sub subtracts numbers', () => {
+    let expected = 7;
+    let result = sub(10, 3);
+    expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+    try {
+        callback();
+        console.log(`✔ ${title}`)
+    } catch (e) {
+        console.error(`X ${title}`);
+        console.error(e);
+    }
+}
 
 function expect(actual) {
     return {
